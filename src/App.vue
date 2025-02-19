faddf09ff7075635b24089b12b0af434f890a6a2
kik8727 <kik87827@gmail.com>
2025-02-20 01:04:23 +0900

    스터디진행중

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
   <div class="menu">
     <a v-for="(item, index) in menus" :key="item">{{ item }}</a>
   </div>
-  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
 
   <div v-for="(item, index) in products" :key="item.title">
+    <img src="./assets/room0.jpg" class="room-img" alt="">
     <h4>{{ item.title }}</h4>
     <p>{{ item.price }}</p>
+    <div>
+      <button @click="countUp(index)">허위매물신고</button> <span>신고수 : {{ count[index] }}</span>
+    </div>
   </div>
 </template>
 <style scope>
@@ -22,6 +26,7 @@ body {
   color: white;
   padding: 10px;
 }
+.room-img{width:100%; margin-top:40px;}
 </style>
 
 <script>
@@ -38,8 +43,20 @@ export default {
         { title: "마포구원룸", price: "가격은아무거나" },
       ],
       menus: ["Home", "Products", "About"],
+      count : []
     };
   },
   components: {},
+  methods: {
+    countUp(index){
+      this.count[index] += 1;
+    }
+  },
+  mounted() {
+   
+  },
+  created() {
+    this.products.forEach(()=> this.count.push(0));
+  },
 };
 </script>
