33a443ab734bdc8b0d89fad854de7964f0dfa817
kik <kik87827@gmail.com>
2025-02-19 17:09:31 +0900

    vue 다시 시작

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
   <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-  <div></div>
+  <div v-for="(item, index) in products" :key="item.title">
+    <h4>{{ item.title }}</h4>
+    <p>{{ item.price }}</p>
+  </div>
 </template>
+<style scope>
+body {
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      price1: 60,
+      price2: 70,
+      style: "color : red",
+      products: [
+        { title: "역삼동원룸", price: "50만원" },
+        { title: "천호동원룸", price: "가격은아무거나" },
+        { title: "마포구원룸", price: "가격은아무거나" },
+      ],
+    };
+  },
+  components: {},
+};
+</script>
