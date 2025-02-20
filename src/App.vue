923a0cbdeb463dfc4b116e8a38677c0fadca2b3c
kik <kik87827@gmail.com>
2025-02-20 11:23:52 +0900

    스터디 진행중

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
+  <div class="black-bg" v-if="modalToggle">
+    <div class="white-bg">
+      <img :src="`/room${modalIndex}.jpg`" class="room-img" alt="" />
+      <h4>{{ products[modalIndex].title }}</h4>
+      <p>{{ products[modalIndex].price }}</p>
+      <div><button @click="modalToggle = false">닫기</button></div>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(item, index) in menus" :key="item">{{ item }}</a>
   </div>
   <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
 
-  <div v-for="(item, index) in products" :key="item.title">
-    <img src="./assets/room0.jpg" class="room-img" alt="">
-    <h4>{{ item.title }}</h4>
+  <div v-for="(item, index) in products" :key="item.title" class="room-list">
+    <img :src="`/room${index}.jpg`" class="room-img" alt="" />
+    <h4 @click="modalOpen(index)">{{ item.title }}</h4>
     <p>{{ item.price }}</p>
     <div>
       <button @click="countUp(index)">허위매물신고</button> <span>신고수 : {{ count[index] }}</span>
@@ -15,18 +24,44 @@
 </template>
 <style scope>
 body {
-  text-align: center;
+  margin: 0;
 }
 .menu {
   background: darkslateblue;
   padding: 15px;
   border-radius: 5px;
+  text-align: center;
 }
 .menu a {
   color: white;
   padding: 10px;
 }
-.room-img{width:100%; margin-top:40px;}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+.room-list {
+  text-align: center;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+  box-sizing: border-box;
+  top: 0;
+  left: 0;
+}
+.white-bg {
+  width: 100%;
+  background: #fff;
+  border-radius: 8px;
+  box-sizing: border-box;
+  padding: 20px;
+  text-align: center;
+}
 </style>
 
 <script>
@@ -43,20 +78,24 @@ export default {
         { title: "마포구원룸", price: "가격은아무거나" },
       ],
       menus: ["Home", "Products", "About"],
-      count : []
+      count: [],
+      modalToggle: false,
+      modalIndex: 0,
     };
   },
   components: {},
   methods: {
-    countUp(index){
+    countUp(index) {
       this.count[index] += 1;
-    }
+    },
+    modalOpen(index) {
+      this.modalToggle = true;
+      this.modalIndex = index;
+    },
   },
-  mounted() {
-   
-  },
+  mounted() {},
   created() {
-    this.products.forEach(()=> this.count.push(0));
+    this.products.forEach(() => this.count.push(0));
   },
 };
 </script>
